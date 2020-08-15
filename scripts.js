1af32af75a4ee8479f3e6b6e91ab0cdfe3b9274f
moshiurshimul <hello@moshiurshimul.com>
2020-08-16 03:21:26 +0600

    style added

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
-fetch('https://www.boredapi.com/api/activity/')
-.then(res => res.json())
-.then(data => {
+function doSomething() {
+    fetch('https://www.boredapi.com/api/activity/')
+    .then(res => res.json())
+    .then(data => {
     document.getElementById('activity').innerText = data.activity;
 })
+}
+doSomething();
+
+setInterval(() => {
+    doSomething(); 
+},10000);
