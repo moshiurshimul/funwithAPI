7d07bf1aad012878c8fc65505936c9b8e0d400c9
moshiurshimul <hello@moshiurshimul.com>
2020-08-16 03:27:50 +0600

    complete

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ function doSomething() {
     fetch('https://www.boredapi.com/api/activity/')
     .then(res => res.json())
     .then(data => {
+        console.log(data)
     document.getElementById('activity').innerText = data.activity;
 })
 }
